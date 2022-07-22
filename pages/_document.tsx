ca2889543b64bb5de57428db73fac64e586dd242
Om Moradia <moradiaom1@gmail.com>
2022-07-22 18:09:34 -0500

    removed viewport meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,15 @@ const Document = () => {
   return (
     <Html>
       <Head>
+        <meta name="description" content="Om Moradia's Portfolio" />
+        <meta name="author" content="Om Moradia" />
+        <meta property="og:title" content="Om Moradia's Website" />
+        <meta property="og:type" content="website" />
+        <link rel="icon" href="/images/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;400;500;700&family=Varela+Round&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="/images/favicon.ico" />
       </Head>
       <body>
         <Main />
