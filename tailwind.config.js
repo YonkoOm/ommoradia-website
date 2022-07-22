4d1742ed188053b77fe9b4d162b30e22c30844cc
Om Moradia <moradiaom1@gmail.com>
2022-07-21 22:40:51 -0500

    added custom styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontFamily: {
+      mplus: ["'M PLUS Rounded 1c'", "sans-serif"],
+      coolvetica: ["Coolvetica", "sans-serif"],
+      varela: ["Varela Round"],
+    },
     extend: {},
   },
   plugins: [],
