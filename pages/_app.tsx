f82ca9ad109f926802cb74b7594cc651ea36f7d7
Om Moradia <moradiaom1@gmail.com>
2022-07-25 17:02:39 -0500

    added head and meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import Navbar from "../components/navbar";
-import { AnimatePresence } from "framer-motion";
-import Spline from "@splinetool/react-spline";
-import { useEffect, useState } from "react";
-import Head from "next/head";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Navbar from '../components/navbar';
+import { AnimatePresence } from 'framer-motion';
+import Spline from '@splinetool/react-spline';
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps, router }: AppProps) => {
   const [show, setShow] = useState(false);
@@ -20,13 +20,18 @@ const MyApp = ({ Component, pageProps, router }: AppProps) => {
   return (
     <div className="w-full h-full absolute">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Om Moradia's Website" />
+        <meta name="author" content="Om Moradia" />
+        <meta property="og:site_name" content="Om Moradia" />
+        <meta property="og:title" content="Om Moradia" />
+        <meta property="og:type" content="website" />
       </Head>
       <Navbar />
       <AnimatePresence exitBeforeEnter>
         <Component {...pageProps} key={router.route} />
       </AnimatePresence>
-      <div className="absolute overflow-hidden h-full w-full">
+      <div className="absolute h-full w-full">
         {show && (
           <Spline scene="https://prod.spline.design/f2GotmeTbUgCOD5f/scene.splinecode" />
         )}
