1c737c403d7f7f9394b03a075142ec783a89419b
Om Moradia <moradiaom1@gmail.com>
2023-06-19 15:40:59 -0500

    added more freedom to add path styling and updated way to obtain current path

--- components/nav-link.tsx
+++ components/nav-link.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { usePathname } from 'next/navigation';
 
 interface Props {
   href: string;
-  className: string;
+  className?: string;
+  pathStyling?: string;
   children: React.ReactNode;
 }
 
-const NavLink: React.FC<Props> = ({ href, children, ...props }) => {
-  const { asPath } = useRouter();
+const NavLink: React.FC<Props> = ({
+  href,
+  className: styling,
+  pathStyling,
+  children
+}) => {
+  const pathname = usePathname();
   return (
     <Link
       href={href}
-      {...props}
-      style={{ color: href === asPath ? '#64ffda' : undefined }}
+      className={
+        href === pathname && styling && pathStyling
+          ? styling.concat(' ', pathStyling)
+          : styling
+      }
+      style={{
+        color: href === pathname ? '#64ffda' : undefined
+      }}
     >
       {children}
     </Link>
