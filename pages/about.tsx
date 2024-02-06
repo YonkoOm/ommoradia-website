f3be375de139e8b154637c0cf9a2439bf5b93d74
Om Moradia <moradiaom1@gmail.com>
2024-02-05 20:37:29 -0600

    moved headshot to homepage and updated styling

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Section from '../components/layout';
 import Link from 'next/link';
-import Image from 'next/image';
 import { motion, Variants } from 'framer-motion';
 
 const About: React.FC = () => {
@@ -11,83 +10,62 @@ const About: React.FC = () => {
     exit: { opacity: 0, x: -40, transition: { duration: 0.5, delay: 0.3 } }
   };
 
-  const imageVariant: Variants = {
-    hidden: { opacity: 0, y: 40 },
-    enter: { opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.6 } },
-    exit: { opacity: 0, x: 40, transition: { duration: 0.5, delay: 0.6 } }
-  };
-
   return (
-    <>
-      <motion.div
-        initial="hidden"
-        animate="enter"
-        exit="exit"
-        whileHover={{
-          x: 20,
-          transition: { type: 'spring', stiffness: 425, damping: 8 }
-        }}
-        variants={imageVariant}
-        className="hidden md:block absolute top-20 left-40 w-[135px] h-[135px] z-10"
-      >
-        <Image src="/images/headshot.png" alt="headshot" sizes="135px" fill />
-      </motion.div>
-      <Section
-        title="About - Om Moradia"
-        header="/about me"
-        variants={headerVariant}
-        whileHover={{
-          scale: 1.1
-        }}
-        whileTap={{
-          scale: 0.9
-        }}
-        transition={{ type: 'spring', stiffness: 500, damping: 7 }}
-      >
-        <p>
-          I am a senior studying <strong>Computer Science</strong> at the{' '}
-          <motion.a
-            href="https://www.wisc.edu/about/"
-            target="_blank"
-            rel="noreferrer noopener"
-            className="text-[#64ffda] sm:inline-flex hover:text-[#bfdbfe]"
-            whileHover={{ scale: 1.01 }}
-          >
-            University of Wisconsin-Madison
-          </motion.a>{' '}
-        </p>
-        <p className="mt-2 md:mt-3">
-          Currently, my main focus is on continuously improving as a programmer
-          and eventually reaching a level where I can solve problems efficiently
-          and implement their solutions without relying too heavily on external
-          sources. To reach this goal, I aim to consistently work on projects
-          that can help diversify my thought process, enable me to learn
-          different technologies, and further refine my{' '}
-          <Link
-            href="/skills"
-            className="text-[#64ffda] sm:inline-flex hover:text-[#bfdbfe] hover:scale-[1.02] ease-in-out duration-[125ms]"
-          >
-            existing knowledge
-          </Link>
-          {'.'}
-        </p>
-        <p className="mt-2 md:mt-3">
-          Apart from this, I am{' '}
-          <strong>
-            actively seeking software engineering positions or internships
-          </strong>{' '}
-          in order to advance my abilities as a programmer and be able to make a
-          meaningful impact on the daily lives of ordinary people like me.
-        </p>
-        <p className="mt-2 md:mt-3">
-          In my free time, and aside from coding, I enjoy trying different types
-          of foods, watching anime and Korean dramas, and listening to music. I
-          also enjoy reading the news, especially anything related to science
-          and the international world, and keeping up with what&apos;s going on
-          in the everchanging programming world.
-        </p>
-      </Section>
-    </>
+    <Section
+      title="About - Om Moradia"
+      header="/about me"
+      variants={headerVariant}
+      whileHover={{
+        scale: 1.1
+      }}
+      whileTap={{
+        scale: 0.9
+      }}
+      transition={{ type: 'spring', stiffness: 500, damping: 7 }}
+    >
+      <p>
+        I am a senior studying <strong>Computer Science</strong> at the{' '}
+        <motion.a
+          href="https://www.wisc.edu/about/"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="text-[#64ffda] sm:inline-flex hover:text-[#bfdbfe]"
+          whileHover={{ scale: 1.01 }}
+        >
+          University of Wisconsin-Madison
+        </motion.a>{' '}
+      </p>
+      <p className="mt-2 md:mt-3">
+        Currently, my main focus is on continuously improving as a programmer
+        and eventually reaching a level where I can solve problems efficiently
+        and implement their solutions without relying too heavily on external
+        sources. To reach this goal, I aim to consistently work on projects that
+        can help diversify my thought process, enable me to learn different
+        technologies, and further refine my{' '}
+        <Link
+          href="/skills"
+          className="text-[#64ffda] sm:inline-flex hover:text-[#bfdbfe] hover:scale-[1.02] ease-in-out duration-[125ms]"
+        >
+          existing knowledge
+        </Link>
+        {'.'}
+      </p>
+      <p className="mt-2 md:mt-3">
+        Apart from this, I am{' '}
+        <strong>
+          actively seeking software engineering positions or internships
+        </strong>{' '}
+        in order to advance my abilities as a programmer and be able to make a
+        meaningful impact on the daily lives of ordinary people like me.
+      </p>
+      <p className="mt-2 md:mt-3">
+        In my free time, and aside from coding, I enjoy trying different types
+        of foods, watching anime and Korean dramas, and listening to music. I
+        also enjoy reading the news, especially anything related to science and
+        the international world, and keeping up with what&apos;s going on in the
+        everchanging programming world.
+      </p>
+    </Section>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
-import { motion, Transition, Variant } from 'framer-motion';
+import Image from 'next/image';
+import { motion, Transition, Variant, Variants } from 'framer-motion';
 import AnimatedText from '../components/animated-text';
 import Typewriter from 'typewriter-effect';
 
@@ -33,6 +34,12 @@ const Home: React.FC = () => {
     repeatType: 'reverse'
   };
 
+  const imageVariant: Variants = {
+    hidden: { opacity: 0, y: 40 },
+    enter: { opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.6 } },
+    exit: { opacity: 0, y: -40, transition: { duration: 0.5, delay: 0.6 } }
+  };
+
   const topLine = 'Welcome.';
   const middleLine = "I'm Om Moradia,";
   const bottomLine = 'aspiring software engineer.';
@@ -40,87 +47,97 @@ const Home: React.FC = () => {
   const baseDelay = 0.4;
 
   return (
-    <motion.div
-      className="absolute my-0 mx-auto top-36 md:top-[35%] left-[10%] z-20"
-      exit={exitAnimation}
-    >
+    <div className="flex flex-col-reverse md:block absolute top-36 md:top-[50%] md:-translate-y-[75%] left-[10%] z-20">
       <Head>
         <title>Om Moradia - Homepage</title>
       </Head>
-      <h1 className="font-coolvetica font-normal text-[28px] sm:text-[36px] md:text-[40px] lg:text-[52px] leading-tight lg:leading-[57px]">
-        <AnimatedText
-          text={topLine}
-          delay={baseDelay}
-          whileHover={letterHover}
-          whileTap={letterTap}
-          transition={letterTransition}
-          letterAnimation={{
-            color: ['#fcdff5', '#9bb3fa', '#ffca28'],
-            transition: colorTransition
-          }}
-        />
-        <AnimatedText
-          text={middleLine}
-          delay={0.075 * (topLine.length + 1) + baseDelay}
-          whileHover={letterHover}
-          whileTap={letterTap}
-          transition={letterTransition}
-          className="hover:text-[#64ffda] text-[#fff7ed]"
-        />
-        <AnimatedText
-          text={bottomLine}
-          delay={0.075 * (topLine.length + middleLine.length + 1) + baseDelay}
-          whileHover={letterHover}
-          whileTap={letterTap}
-          transition={letterTransition}
-          letterAnimation={{
-            color: ['#ffca28', '#9bb3fa', '#fcdff5'],
-            transition: colorTransition
-          }}
-        />
-      </h1>
-      <h2 className="font-mplus font-normal text-[#64ffda] space-x-1.5 sm:mt-0.5 md:mt-1 text-[15px] sm:text-[20px] lg:text-[21px]">
-        <Typewriter
-          onInit={typewriter => {
-            typewriter
-              .pauseFor(2500)
-              .typeString("what's up, doc?")
-              .pauseFor(1500)
-              .deleteAll()
-              .pauseFor(400)
-              .typeString('as you already know, my name is Om.')
-              .pauseFor(1000)
-              .deleteAll()
-              .pauseFor(400)
-              .typeString("and yes... it's only two letters long.")
-              .pauseFor(1000)
-              .deleteAll()
-              .pauseFor(400)
-              .typeString("i'm a student.")
-              .pauseFor(1000)
-              .deleteAll()
-              .pauseFor(400)
-              .typeString('programmer.')
-              .pauseFor(1000)
-              .deleteAll()
-              .pauseFor(400)
-              .typeString('music connoisseur.')
-              .pauseFor(1000)
-              .deleteAll()
-              .pauseFor(400)
-              .typeString('and an anime & k-drama enthusiast.')
-              .pauseFor(1000)
-              .deleteAll()
-              .pauseFor(400)
-              .typeString('welcome to my page :)')
-              .pauseFor(1000)
-              .deleteAll()
-              .start();
-          }}
-          options={{ loop: true, deleteSpeed: 10, delay: 60 }}
-        />
-      </h2>
-    </motion.div>
+      <motion.div
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        whileHover={{ scale: 1.2, rotate: 360 }}
+        transition={{ duration: 0.5 }}
+        variants={imageVariant}
+        className="relative mx-auto md:mx-0 mt-14 md:mt-0 md:mb-5 w-[105px] h-[105px] md:w-[120px] md:h-[120px]"
+      >
+        <Image src="/images/headshot.png" alt="headshot" sizes="135px" fill />
+      </motion.div>
+      <motion.div exit={exitAnimation}>
+        <h1 className="font-coolvetica font-normal text-[28px] sm:text-[36px] md:text-[40px] lg:text-[52px] leading-tight lg:leading-[57px]">
+          <AnimatedText
+            text={topLine}
+            delay={baseDelay}
+            whileHover={letterHover}
+            whileTap={letterTap}
+            transition={letterTransition}
+            letterAnimation={{
+              color: ['#fcdff5', '#9bb3fa', '#ffca28'],
+              transition: colorTransition
+            }}
+          />
+          <AnimatedText
+            text={middleLine}
+            delay={0.08 * (topLine.length + 1) + baseDelay}
+            whileHover={letterHover}
+            whileTap={letterTap}
+            transition={letterTransition}
+            className="text-[#fff7ed] hover:text-[#64ffda]"
+          />
+          <AnimatedText
+            text={bottomLine}
+            delay={0.08 * (topLine.length + middleLine.length + 1) + baseDelay}
+            whileHover={letterHover}
+            whileTap={letterTap}
+            transition={letterTransition}
+            letterAnimation={{
+              color: ['#ffca28', '#9bb3fa', '#fcdff5'],
+              transition: colorTransition
+            }}
+          />
+        </h1>
+        <h2 className="font-mplus font-normal text-[#64ffda] space-x-1.5 sm:mt-0.5 md:mt-1 text-[15px] sm:text-[20px] lg:text-[21px]">
+          <Typewriter
+            onInit={typewriter => {
+              typewriter
+                .pauseFor(2500)
+                .typeString("what's up, doc?")
+                .pauseFor(1500)
+                .deleteAll()
+                .pauseFor(400)
+                .typeString('as you already know, my name is Om.')
+                .pauseFor(1000)
+                .deleteAll()
+                .pauseFor(400)
+                .typeString("and yes... it's only two letters long.")
+                .pauseFor(1000)
+                .deleteAll()
+                .pauseFor(400)
+                .typeString("i'm a student.")
+                .pauseFor(1000)
+                .deleteAll()
+                .pauseFor(400)
+                .typeString('programmer.')
+                .pauseFor(1000)
+                .deleteAll()
+                .pauseFor(400)
+                .typeString('music connoisseur.')
+                .pauseFor(1000)
+                .deleteAll()
+                .pauseFor(400)
+                .typeString('and an anime & k-drama enthusiast.')
+                .pauseFor(1000)
+                .deleteAll()
+                .pauseFor(400)
+                .typeString('welcome to my page :)')
+                .pauseFor(1000)
+                .deleteAll()
+                .start();
+            }}
+            options={{ loop: true, deleteSpeed: 10, delay: 60 }}
+          />
+        </h2>
+      </motion.div>
+    </div>
   );
 };
 
